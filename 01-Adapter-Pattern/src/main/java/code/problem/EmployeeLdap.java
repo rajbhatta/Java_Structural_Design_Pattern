0b069bd9526f7a1a55829ddf4e2c54d33a4dd2aa
Raj Bhatta <bhattaraj21@gmail.com>
2020-09-07 20:10:50 -0700

    Why we need Adapter Design Pattern

--- 01-Adapter-Pattern/src/main/java/code/problem/EmployeeLdap.java
+++ 01-Adapter-Pattern/src/main/java/code/problem/EmployeeLdap.java
@@ -1,5 +1,31 @@
 package code.problem;
 
 public class EmployeeLdap {
+        private String cn;
+        private String surname;
+        private String givenName;
+        private String email;
 
+    public EmployeeLdap(String cn, String surname, String givenName, String email) {
+        this.cn = cn;
+        this.surname = surname;
+        this.givenName = givenName;
+        this.email = email;
+    }
+
+    public String getCn() {
+        return cn;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
