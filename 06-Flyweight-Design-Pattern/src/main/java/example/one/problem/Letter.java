f1c42dea09ab0cbd28dfe9aa391582f566ab6a36
Raj Bhatta <bhattaraj21@gmail.com>
2020-09-09 13:03:33 -0700

    Flyweight design pattern demo

--- 06-Flyweight-Design-Pattern/src/main/java/example/one/problem/Letter.java
+++ 06-Flyweight-Design-Pattern/src/main/java/example/one/problem/Letter.java
@@ -5,7 +5,9 @@ public class Letter {
     private String value;
 
     public Letter(String value) {
+
         this.value = value;
+        System.out.println("New letter created with value: \t"+value);
     }
 
     public String getValue() {
